0aaf7ef68770067d4af5e88b9180f92af0a05c4b
spycrowsoft <spycrowsoft>
2020-04-25 22:19:19 +0200

    Updated sql schema

--- backend/COVID-backend.sql
+++ backend/COVID-backend.sql
@@ -35,9 +35,9 @@ CREATE TABLE IF NOT EXISTS account_admins (
 	username VARCHAR(32) NOT NULL, -- Unique username of healthcare_worker
 	hashed_password VARCHAR(64) NOT NULL, -- Salted password hash.
 	salt VARCHAR(32) NOT NULL, -- account_admin specific salt for password.
-	totp_seed VARCHAR(32) NOT NULL, -- account_admin specific seed for HMAC-TOTP authentication.
-	email VARCHAR(255) NOT NULL, -- E-mail address of healthcare_worker.
-	phone_number VARCHAR(20) NOT NULL, -- Phone number on which the account_admin has to be near-instantly available so we can reach them if their account shows suspicious activity.
+	totp_seed VARCHAR(32) NULL DEFAULT NULL, -- account_admin specific seed for HMAC-TOTP authentication.
+	email VARCHAR(255) NULL, -- E-mail address of healthcare_worker.
+	phone_number VARCHAR(20) NULL, -- Phone number on which the account_admin has to be near-instantly available so we can reach them if their account shows suspicious activity.
 	reset_code VARCHAR(64) NULL DEFAULT NULL, -- Password reset code.
 	active BOOLEAN NOT NULL DEFAULT FALSE, -- Is account active?
 	account_expiration_date DATE NOT NULL DEFAULT DATE_ADD(CURDATE(), INTERVAL 2 YEAR), -- Automatic expiration after 2 years.
@@ -53,12 +53,22 @@ CREATE TABLE IF NOT EXISTS account_admins (
 
 CREATE TABLE IF NOT EXISTS account_admin_sessions (
 	account_admin_uuid VARCHAR(36) NOT NULL, -- account_admin_uuid of session
-	active BOOLEAN NOT NULL DEFAULT TRUE, -- Wether or not this session is active.
+	active BOOLEAN NOT NULL DEFAULT FALSE, -- Wether or not this session is active.
 	session_token VARCHAR(36) NOT NULL, -- Token used to identify a session.
 	begin_time TIMESTAMP NOT NULL DEFAULT NOW(), -- Time of session start.
 	expiration_time TIMESTAMP NOT NULL DEFAULT DATE_ADD(NOW(), INTERVAL 30 MINUTE), -- Timestamp after which a session naturally expires if it has not been used.
-	PRIMARY KEY (session_token),
-	KEY (session_token, account_admin_uuid),
+	PRIMARY KEY (account_admin_uuid, session_token),
+	FOREIGN KEY (account_admin_uuid) REFERENCES account_admins(account_admin_uuid) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+--	The account_admin_actions table contains a log of all the actions performed by account_admins
+
+CREATE TABLE IF NOT EXISTS account_admin_actions (
+	account_admin_uuid VARCHAR(36) NOT NULL, -- account_admin_uuid of session.
+	action_time TIMESTAMP NOT NULL DEFAULT NOW(), -- Time at which the action is performed.
+	action_type VARCHAR(32) NOT NULL, -- The action performed.
+	affected_uuid VARCHAR(36) NOT NULL, -- Unique UUID of healthcare_worker or account_admin the action is performed on.
+	details_log TEXT NOT NULL, -- Detailed logging information of what the account_admin did.
 	FOREIGN KEY (account_admin_uuid) REFERENCES account_admins(account_admin_uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -75,9 +85,9 @@ CREATE TABLE IF NOT EXISTS healthcare_workers (
 	username VARCHAR(32) NOT NULL, -- Unique username of healthcare_worker
 	hashed_password VARCHAR(64) NOT NULL, -- Salted password hash.
 	salt VARCHAR(32) NOT NULL, -- healthcare_worker specific salt for password.
-	totp_seed VARCHAR(32) NOT NULL, -- healthcare_worker specific seed for HMAC-TOTP authentication.
-	email VARCHAR(255) NOT NULL, -- E-mail address of healthcare_worker.
-	phone_number VARCHAR(20) NOT NULL, -- Phone number on which the healthcare_worker has to be near-instantly available so we can reach them if their account shows suspicious activity.
+	totp_seed VARCHAR(32) NULL DEFAULT NULL, -- healthcare_worker specific seed for HMAC-TOTP authentication.
+	email VARCHAR(255) NULL DEFAULT NULL, -- E-mail address of healthcare_worker.
+	phone_number VARCHAR(20) NULL DEFAULT NULL, -- Phone number on which the healthcare_worker has to be near-instantly available so we can reach them if their account shows suspicious activity.
 	reset_code VARCHAR(64) NULL DEFAULT NULL, -- Password reset code.
 	active BOOLEAN NOT NULL DEFAULT FALSE, -- Is account active?
 	account_expiration_date DATE NOT NULL DEFAULT DATE_ADD(CURDATE(), INTERVAL 2 YEAR), -- Automatic expiration after 2 years.
@@ -89,21 +99,11 @@ CREATE TABLE IF NOT EXISTS healthcare_workers (
 	UNIQUE (phone_number)
 );
 
---	The account_admin_actions table contains a log of all the actions performed by account_admins
-
-CREATE TABLE IF NOT EXISTS account_admin_actions (
-	account_admin_uuid VARCHAR(36) NOT NULL, -- account_admin_uuid of session.
-	action_time TIMESTAMP NOT NULL DEFAULT NOW(), -- Time at which the action is performed.
-	action_type VARCHAR(32) NOT NULL, -- The action performed.
-	healthcare_worker_uuid VARCHAR(36) NOT NULL, -- Unique UUID of healthcare_worker the action is performed on.
-	FOREIGN KEY (account_admin_uuid) REFERENCES account_admins(account_admin_uuid) ON UPDATE CASCADE ON DELETE CASCADE
-);
-
 -- A table which keeps track of all heelthcare_worker logins
 
 CREATE TABLE IF NOT EXISTS healthcare_worker_sessions (
 	healthcare_worker_uuid VARCHAR(36) NOT NULL, -- healthcare_worker_uuid of session
-	active BOOLEAN NOT NULL DEFAULT TRUE, -- Wether or not this session is active.
+	active BOOLEAN NOT NULL DEFAULT FALSE, -- Wether or not this session is active.
 	session_token VARCHAR(36) NOT NULL, -- Token used to identify a session.
 	begin_time TIMESTAMP NOT NULL DEFAULT NOW(), -- Time of session start.
 	expiration_time TIMESTAMP NOT NULL DEFAULT DATE_ADD(NOW(), INTERVAL 30 MINUTE), -- Timestamp after which a session naturally expires if it has not been used.
@@ -114,8 +114,8 @@ CREATE TABLE IF NOT EXISTS healthcare_worker_sessions (
 
 CREATE TABLE IF NOT EXISTS daily_tracing_key_activation_requests (
 	request_uuid VARCHAR(36) NOT NULL DEFAULT UUID(), -- UUID of activation request.
-	healthcare_worker_uuid VARCHAR(36) NULL, -- UUID of healthcare_worker who made the activation request.
-	valid_until TIMESTAMP NOT NULL DEFAULT DATE_ADD(NOW(), INTERVAL 2 HOUR), -- Expiration timestamp of the healthcare_worker's session.
+	healthcare_worker_uuid VARCHAR(36) NOT NULL, -- UUID of healthcare_worker who made the activation request.
+	valid_until TIMESTAMP NOT NULL DEFAULT DATE_ADD(NOW(), INTERVAL 14 DAY), -- Expiration timestamp of the healthcare_worker's session.
 	request_token VARCHAR(36) NOT NULL, -- Single use token which can be used to submit daily_tracing_keys.
 	time_received TIMESTAMP NULL DEFAULT NULL, -- Time at which the daily_tracing_keys have been received.
 	PRIMARY KEY (request_uuid),
@@ -126,10 +126,10 @@ CREATE TABLE IF NOT EXISTS daily_tracing_key_activation_requests (
 
 CREATE TABLE IF NOT EXISTS active_daily_tracing_keys (
 	request_uuid VARCHAR(36) NOT NULL, -- UUID of activation request.
-	day_number DATE NOT NULL, -- Day number belonging to the daily_tracing_key.
+	interval_number UNSIGNED INTEGER NOT NULL, -- Day number belonging to the daily_tracing_key.
 	daily_tracing_key BINARY(16) NOT NULL, -- daily_tracing_key.
-	PRIMARY KEY(request_uuid, day_number, daily_tracing_key),
-	KEY (day_number, daily_tracing_key),
+	PRIMARY KEY(request_uuid, interval_number, daily_tracing_key),
+	KEY (interval_number, daily_tracing_key),
 	FOREIGN KEY (request_uuid) REFERENCES daily_tracing_key_activation_requests(request_uuid) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
@@ -138,11 +138,11 @@ CREATE TABLE IF NOT EXISTS retracted_daily_tracing_keys (
 	healthcare_worker_uuid VARCHAR(36) NOT NULL, -- uuid of the healthcare_worker who issued the retraction event.
 	activation_request_uuid VARCHAR(36) NULL, -- uuid of the activation request the retracted daily_tracing_key appeared in.
 	time_of_retraction TIMESTAMP NOT NULL DEFAULT NOW(), -- timestamp of when the retraction was issued.
-	day_number DATE NOT NULL, -- Day number belonging to the daily_tracing_key.
+	interval_number UNSIGNED INTEGER NOT NULL, -- Day number belonging to the daily_tracing_key.
 	daily_tracing_key BINARY(16) NOT NULL, -- daily_tracing_key.
 	PRIMARY KEY (retraction_uuid),
 	FOREIGN KEY (healthcare_worker_uuid) REFERENCES healthcare_workers(healthcare_worker_uuid) ON UPDATE CASCADE ON DELETE NO ACTION,
-	FOREIGN KEY (day_number, daily_tracing_key) REFERENCES active_daily_tracing_keys(day_number, daily_tracing_key) ON UPDATE CASCADE ON DELETE NO ACTION,
+	FOREIGN KEY (interval_number, daily_tracing_key) REFERENCES active_daily_tracing_keys(interval_number, daily_tracing_key) ON UPDATE CASCADE ON DELETE NO ACTION,
 	FOREIGN KEY (activation_request_uuid) REFERENCES active_daily_tracing_keys(request_uuid) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
@@ -169,7 +169,7 @@ CREATE OR REPLACE VIEW view_daily_tracing_key_submitted_by_healthcare_workers AS
 SELECT
 	healthcare_workers.username AS username,
 	dtkars.time_received AS time_received,
-	adtks.day_number AS day_number,
+	adtks.interval_number AS interval_number,
     adtks.daily_tracing_key AS daily_tracing_key
 FROM
 	healthcare_workers,
